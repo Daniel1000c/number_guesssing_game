441020f08d2112e7cf1b019ae4d8cdacfc06f1fa
Daniel Velez <velezdaniel516@gmail.com>
2025-08-09 15:25:36 -0400

    add script template file and added check button event listenter"

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//score counter
+let score = 40;
+
+//Create highscore counter
+let high_score = 0;
+
+//Create try counter
+let tries = 10;
+
+//Create check button function
+document.getElementById("checkNumber").addEventListener("click", function () {
+  //Check user value
+  const userGuess = Number(document.getElementById("guess").value);
+
+  //Check if the user has put a number in or not
+});
