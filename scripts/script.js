cd04278162ecd90579fbfde3fdf2a8b6bbd840cb
Daniel Velez <velezdaniel516@gmail.com>
2025-08-09 15:36:29 -0400

    add error message when no input is detected

--- scripts/script.js
+++ scripts/script.js
@@ -9,10 +9,27 @@ let high_score = 0;
 //Create try counter
 let tries = 10;
 
+//Create display message function
+const displayMessage = function (message) {
+  //Get message id
+  const result = (document.getElementById("gameMessage").textContent = message);
+
+  //Return result
+  return result;
+};
+
 //Create check button function
 document.getElementById("checkNumber").addEventListener("click", function () {
   //Check user value
   const userGuess = Number(document.getElementById("guess").value);
 
   //Check if the user has put a number in or not
+  if (!userGuess) {
+    //Display message that user must input a number
+    displayMessage("You must input a number between 1 and 40!!!");
+
+    //Change text color to red when this happens
+    document.getElementById("gameMessage").style.color = "#FF0000";
+    document.getElementById("gameMessage").style.fontWeight = "bolder";
+  }
 });
